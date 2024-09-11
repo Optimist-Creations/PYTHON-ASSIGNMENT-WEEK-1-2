09effb565d9f82dff29df9889baa5ccc97457a24
Optimist-Creations <salaudeenmubaraq20@gmail.com>
2024-09-11 10:44:13 +0100

    Add files via upload
    Python assignment files

---
+++ Variable Redeclaration.py
@@ -0,0 +1,8 @@
+# Declaration of Variable 
+Age = 34
+Name = "Salaudeen Mubaraq Olansile"
+print("Hello, Salaudeen Mubaraq Olansile, your Age =", Age)
+
+# Variable Redeclaration  
+Age = 64
+print("Welcome, Salaudeen Mubaraq Olansile, your Age =", Age)
